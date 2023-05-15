47f5f1589d3fb6d8b60a2c803475e93523646c5a
Vadim220409 <107919314+Vadim220409@users.noreply.github.com>
2023-05-15 19:22:07 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// ex 1
+
+var numbers1 = [1, 2, 3, 4, 5];
+var newArea1 = numbers1.map(function(numbers1) {
+  return numbers1 * 100;
+});
+
+console.log(newArea1);
+
+// ex 2
+
+var line = ["Привіт", "Добрий день", "Бувай"];
+var newArea2 = line.map(function(line) {
+  return line.length;
+});
+
+console.log(newArea2);
+
+// ex 3
+
+var numbers2 = [50, 120, 80, 200, 30];
+var newArea2 = числа.filter(function(numbers2) {
+  return numbers2 <= 100;
+});
+
+console.log(newArea2);
